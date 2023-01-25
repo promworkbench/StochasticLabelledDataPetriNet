a7083bae16b03ffdb6a1c110887deabafcc3fac2
sleemans <s.leemans@bpm.rwth-aachen.de>
2023-01-25 11:13:58 +0000

    use encoding in uemsc
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/StochasticLabelledDataPetriNet/Trunk@47073 a68f48d4-8d00-424f-b812-3f22a534a02a

--- src/org/processmining/stochasticlabelleddatapetrinet/plugins/DataUnitEarthMoversStochasticConformancePlugin.java
+++ src/org/processmining/stochasticlabelleddatapetrinet/plugins/DataUnitEarthMoversStochasticConformancePlugin.java
@@ -6,15 +6,21 @@ import org.processmining.framework.plugin.ProMCanceller;
 import org.processmining.stochasticlabelleddatapetrinet.StochasticLabelledDataPetriNetSemantics;
 import org.processmining.stochasticlabelleddatapetrinet.duemsc.duEMSC;
 import org.processmining.stochasticlabelleddatapetrinet.logadapter.DataStateLogAdapterImpl;
+import org.processmining.stochasticlabelleddatapetrinet.preprocess.OneHotEncoding;
 
 import lpsolve.LpSolveException;
 
 public class DataUnitEarthMoversStochasticConformancePlugin {
 
-	public static double measureLogModel(XLog log, XEventClassifier classifier,
-			StochasticLabelledDataPetriNetSemantics semanticsB, boolean debug, ProMCanceller canceller)
-			throws LpSolveException {
-		return duEMSC.compute(log, classifier, new DataStateLogAdapterImpl(semanticsB), semanticsB, canceller);
+	public static double measureLogModel(XLog log, XEventClassifier classifier, SLDPN model, boolean debug,
+			ProMCanceller canceller) throws LpSolveException {
+
+		OneHotEncoding encoder = model.getOneHotEncoding();
+		XLog log2 = encoder.process(log);
+
+		StochasticLabelledDataPetriNetSemantics semanticsB = model.getModel().getDefaultSemantics();
+
+		return duEMSC.compute(log2, classifier, new DataStateLogAdapterImpl(semanticsB), semanticsB, canceller);
 	}
 
 }
