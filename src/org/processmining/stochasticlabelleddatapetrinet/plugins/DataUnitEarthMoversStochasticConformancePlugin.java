9b8b01c31fb08f819fbb4e61c1a3aedfb533aa3f
sleemans <s.leemans@bpm.rwth-aachen.de>
2023-01-27 10:07:38 +0000

    m
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/StochasticLabelledDataPetriNet/Trunk@47098 a68f48d4-8d00-424f-b812-3f22a534a02a

--- src/org/processmining/stochasticlabelleddatapetrinet/plugins/DataUnitEarthMoversStochasticConformancePlugin.java
+++ src/org/processmining/stochasticlabelleddatapetrinet/plugins/DataUnitEarthMoversStochasticConformancePlugin.java
@@ -1,8 +1,16 @@
 package org.processmining.stochasticlabelleddatapetrinet.plugins;
 
 import org.deckfour.xes.classification.XEventClassifier;
+import org.deckfour.xes.classification.XEventNameClassifier;
 import org.deckfour.xes.model.XLog;
+import org.processmining.contexts.uitopia.annotations.UITopiaVariant;
+import org.processmining.framework.plugin.PluginContext;
 import org.processmining.framework.plugin.ProMCanceller;
+import org.processmining.framework.plugin.annotations.Plugin;
+import org.processmining.framework.plugin.annotations.PluginLevel;
+import org.processmining.framework.plugin.annotations.PluginVariant;
+import org.processmining.framework.util.HTMLToString;
+import org.processmining.plugins.InductiveMiner.plugins.dialogs.IMMiningDialog;
 import org.processmining.stochasticlabelleddatapetrinet.StochasticLabelledDataPetriNetSemantics;
 import org.processmining.stochasticlabelleddatapetrinet.duemsc.duEMSC;
 import org.processmining.stochasticlabelleddatapetrinet.logadapter.DataStateLogAdapterImpl;
@@ -11,6 +19,26 @@ import org.processmining.stochasticlabelleddatapetrinet.preprocess.OneHotEncodin
 import lpsolve.LpSolveException;
 
 public class DataUnitEarthMoversStochasticConformancePlugin {
+
+	@Plugin(name = "Check conformance of a stochastic labelled data Petri net and an event log using Data-Aware Unit Earth Movers' Stochastic Conformance duemsc", level = PluginLevel.Regular, returnLabels = {
+			"Stochastic labelled data Petri net" }, returnTypes = { HTMLToString.class }, parameterLabels = {
+					"stochastic labelled data Petri net (SLDPN)", "Log" }, userAccessible = true)
+	@UITopiaVariant(affiliation = IMMiningDialog.affiliation, author = IMMiningDialog.author, email = IMMiningDialog.email)
+	@PluginVariant(variantLabel = "Check an SLDPN, dialog", requiredParameterLabels = { 0, 1 })
+	public HTMLToString mine(PluginContext context, SLDPN model, XLog xLog) throws Exception {
+		ProMCanceller canceller = new ProMCanceller() {
+			public boolean isCancelled() {
+				return context.getProgress().isCancelled();
+			}
+		};
+		double result = measureLogModel(xLog, new XEventNameClassifier(), model, false, canceller);
+		return new HTMLToString() {
+
+			public String toHTMLString(boolean includeHTMLTags) {
+				return "data-aware Earth Movers' Stochastic Conformance: " + result;
+			}
+		};
+	}
 
 	public static double measureLogModel(XLog log, XEventClassifier classifier, SLDPN model, boolean debug,
 			ProMCanceller canceller) throws LpSolveException {
